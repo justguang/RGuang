759bcabe6cc89d45b7dde3e3f8029aa142ec2688
justguang <2320366520@qq.com>
2023-12-10 22:28:55 +0800

    测试RLog

--- Assets/Scripts/LogManager.cs
+++ Assets/Scripts/LogManager.cs
@@ -5,18 +5,19 @@
 /// @description： 日志配置管理                                                                                      
 ///****************************************************************************
 using UnityEngine;
-using RGuang.Utils;
+using RGuang;
+
 public class LogManager : QFramework.PersistentMonoSingleton<LogManager>
 {
-    [Header("启用日志等级"), SerializeField] ULoggerLevel m_logLevel;
+    [Header("启用日志等级"), SerializeField] RLoggerLevel m_logLevel;
     [Header("日志内容显示时间"), SerializeField] bool m_enableTime;
     [Header("日志内容显示线程ID"), SerializeField] bool m_enableThreadID;
     [Header("日志内容保存到文件"), SerializeField] bool m_enableSave;
     [Header("日志文件新内容覆盖原有内容"), SerializeField] bool m_enableSaveCover;
     [Header("日志文件名"), SerializeField] string m_saveFileName = "log.txt";
-    [Header("日志文件保存路劲"), SerializeField] string m_saveFilePath = @"D:\workspace\RGuang\Logs\ULog\";
+    [Header("日志文件保存路劲"), SerializeField] string m_saveFilePath = @"D:\workspace\RGuang\Logs\RLog\";
 
-    readonly ULoggerType m_loggerType = ULoggerType.Unity;
+    readonly RLoggerType m_loggerType = RLoggerType.Unity;
 
 
     protected override void Awake()
@@ -26,12 +27,13 @@ public class LogManager : QFramework.PersistentMonoSingleton<LogManager>
 #if UNITY_EDITOR
         if (!System.IO.Directory.Exists(m_saveFilePath)) System.IO.Directory.CreateDirectory(m_saveFilePath);
 #else
-        string logPath = PathUtils.Path_StreamingAssets + "/ULog/";
+        string logPath = PathUtils.Path_StreamingAssets + "/RLog/";
         if (!System.IO.Directory.Exists(logPath)) System.IO.Directory.CreateDirectory(logPath);
 #endif
 
+        
 
-        ULogConfig cfg = new ULogConfig
+        RLogConfig cfg = new RLogConfig
         {
             logLevel = m_logLevel,
 
@@ -50,11 +52,11 @@ public class LogManager : QFramework.PersistentMonoSingleton<LogManager>
             loggerType = m_loggerType,
         };
 
-        ULog.InitSetting(cfg);
-        ULog.ColorLog(ULogColor.Cyan, "日志初始化完成.");
-        ULog.ColorLog(ULogColor.Cyan, $"日志等级{cfg.logLevel}");
-        ULog.ColorLog(ULogColor.Cyan, $"日志保存路劲{cfg.savePath}");
-        ULog.ColorLog(ULogColor.Cyan, $"日志文件名{cfg.saveName}");
+        RLog.InitSetting(cfg);
+        RLog.ColorLog(RLogColor.Cyan, "日志初始化完成.");
+        RLog.ColorLog(RLogColor.Cyan, $"日志等级{cfg.logLevel}");
+        RLog.ColorLog(RLogColor.Cyan, $"日志保存路劲{cfg.savePath}");
+        RLog.ColorLog(RLogColor.Cyan, $"日志文件名{cfg.saveName}");
     }
 
 }

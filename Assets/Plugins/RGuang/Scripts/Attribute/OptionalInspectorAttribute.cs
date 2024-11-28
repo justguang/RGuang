acb5d2d7553744f051f0290aff642c3d616cc6c4
justguang <2320366520@qq.com>
2024-11-28 18:09:37 +0800

    update optionalInspectorAttribute

--- Assets/Plugins/RGuang/Scripts/Attribute/OptionalInspectorAttribute.cs
+++ Assets/Plugins/RGuang/Scripts/Attribute/OptionalInspectorAttribute.cs
@@ -1,22 +1,61 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RGuang.Attribute
 {
     [System.Serializable]
     public struct OptionalInspector<T>
     {
-        public bool Enabled => enabled;
-        public T Value => value;
+        /// <summary>
+        /// True在Inspector面板上可读写，
+        /// False在Inspector面板上只读
+        /// </summary>
+        public bool Enabled
+        {
+            get => enabled;
+            set => enabled = value;
+        }
+
+        public T Value
+        {
+            get => value;
+            set
+            {
+                if (value.Equals(this.value)) return;
+                T eventArgs = this.value;
+                this.value = value;
+                E_OnValueChanged.Invoke(eventArgs);
+            }
+        }
         [UnityEngine.SerializeField] private bool enabled;
         [UnityEngine.SerializeField] private T value;
-        public OptionalInspector(T initialValue)
+
+        /// <summary>
+        /// 变更值后事件通知：
+        ///     通知参数：变更前的值
+        /// </summary>
+        public UnityEvent<T> E_OnValueChanged;
+
+        public OptionalInspector(T initialValue, UnityAction<T> onValueChangedCallback = null)
         {
             enabled = true;
             value = initialValue;
+            E_OnValueChanged = new UnityEvent<T>();
+            if (onValueChangedCallback != null) E_OnValueChanged.AddListener(onValueChangedCallback);
         }
 
-    }
+        public OptionalInspector(bool enable, T initialValue, UnityAction<T> onValueChangedCallback = null)
+        {
+            enabled = enable;
+            value = initialValue;
+            E_OnValueChanged = new UnityEvent<T>();
+            if (onValueChangedCallback != null) E_OnValueChanged.AddListener(onValueChangedCallback);
+        }
 
+
+
+
+    }
 
 }
